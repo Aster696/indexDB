9a99891edcb5c5fde2bb105c6d0e0aa5710509e4
Aster696 <aster.r@codezen.in>
2023-10-17 13:43:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,26 @@ import { OfflineApiInterceptor } from './service/offline-api-interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { CustomInterceptor } from './service/custom.interceptor';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { WelcomeModule } from './pages/welcome/welcome.module';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
@@ -23,7 +36,13 @@ import { CustomInterceptor } from './service/custom.interceptor';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    FormsModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    WelcomeModule
   ],
   providers: [
     ApiService,
@@ -33,6 +52,7 @@ import { CustomInterceptor } from './service/custom.interceptor';
       useClass: CustomInterceptor,
       multi: true
   },
+    { provide: NZ_I18N, useValue: en_US },
   ],
   bootstrap: [AppComponent]
 })
