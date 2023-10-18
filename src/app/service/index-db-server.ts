0bce79f1d20c576e2ddb141bb30d012ca8ccb627
sufiyan <sufiyan@codezen.in>
2023-10-18 16:11:18 +0530

    delete condition added

--- src/app/service/index-db-server.ts
+++ src/app/service/index-db-server.ts
@@ -8,12 +8,37 @@ export class IndexedDbService {
   private db: Dexie;
 
   constructor() {
-    // this.initDatabase();
+    this.initDatabase();
+  }
+
+  private async initDatabase() {
     this.db = new Dexie('VoterDataset');
     this.db.version(1).stores({
-      voterData: 'id, data',
-      voterActivityData: '++id, data'
+      voterData: '++id, data',
+      // voterActivityData: '++id, data'
     });
+
+    // Check if the table 'voterData' exists
+    const tableExists = await this.tableExists('voterActivityData');
+
+    if (!tableExists) {
+      console.log("table not exist")
+      // Table doesn't exist, so delete the entire database and recreate
+      // await this.deleteDatabase();
+      // this.db.close(); // Close the old database instance
+      // this.initDatabase(); // Recreate the database
+    }
+  }
+
+  private async tableExists(tableName: string): Promise<boolean> {
+    // Check if the table name exists in the database
+    const count = await this.db.table(tableName).where('id').between(0, 1).count();
+    return count === 0;
+  }
+
+  private async deleteDatabase() {
+    // Delete the database
+    await Dexie.delete('VoterDataset');
   }
 
   // private initDatabase() {
